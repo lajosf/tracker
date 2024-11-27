8ad0c05a822468abfab673e08aab4967ac2a6fb2
Lajos Fulep <lajosfulep@icloud.com>
2024-11-27 10:10:38 +0100

    Start to introduce activity repitition type.

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -15,13 +15,13 @@ const LAST_WEEK_DATE = new Date(Date.now() - 7 * 24 * 60 * 60 * 1000).toISOStrin
 
 export const ACTIVITIES: Activity[] = [
 
-    { id: '1', name: 'Today1', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: null },
-    { id: '2', name: 'Yesterday', folder: 'none', done: false, createdAt: YESTERDAY_DATE, deletedAt: null },
-    { id: '3', name: 'Every Tuesday', folder: 'none', done: false, createdAt: A_TUESDAY_IN_THE_PAST, deletedAt: null },
-    { id: '4', name: 'LastWeek1', folder: 'none', done: false, createdAt: LAST_WEEK_DATE, deletedAt: null },
-    { id: '5', name: 'LastWeek2', folder: 'none', done: false, createdAt: LAST_WEEK_DATE, deletedAt: null },
-    { id: '6', name: 'Deleted1', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: new Date().toISOString() },
-    { id: '7', name: 'Deleted2', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: new Date().toISOString() },
+    { id: '1', name: 'Today1', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: null, repetitionType: 'daily' },
+    { id: '2', name: 'Yesterday', folder: 'none', done: false, createdAt: YESTERDAY_DATE, deletedAt: null, repetitionType: 'daily' },
+    { id: '3', name: 'Every Tuesday', folder: 'none', done: false, createdAt: A_TUESDAY_IN_THE_PAST, deletedAt: null, repetitionType: 'weekly' },
+    { id: '4', name: 'LastWeek1', folder: 'none', done: false, createdAt: LAST_WEEK_DATE, deletedAt: null, repetitionType: 'weekly' },
+    { id: '5', name: 'LastWeek2', folder: 'none', done: false, createdAt: LAST_WEEK_DATE, deletedAt: null, repetitionType: 'weekly' },
+    { id: '6', name: 'Deleted1', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: new Date().toISOString(), repetitionType: 'daily' },
+    { id: '7', name: 'Deleted2', folder: 'none', done: false, createdAt: new Date().toISOString(), deletedAt: new Date().toISOString(), repetitionType: 'daily' },
 ];
 
 export default { FOLDERS, ACTIVITIES };
--- src/types/types.ts
+++ src/types/types.ts
@@ -1,7 +1,7 @@
 import { PRESERVED_FOLDERS } from "../constants/preservedFolders";
 
 export type FolderType = 'system' | 'user';
-
+export type RepetitionType = 'daily' | 'weekly' | 'bi-weekly' | 'monthly' | 'nth-day' | 'day-of-week' | 'day-of-month' | 'nth-time-of-day' | 'nth-time-of-week' | 'nth-time-of-month';
 export type PreservedFolderType = typeof PRESERVED_FOLDERS[number];
 
 export interface Folder {
@@ -16,6 +16,7 @@ export interface Activity {
     name: string;
     folder: string;
     done: boolean;
+    repetitionType: RepetitionType;
     completedAt?: string | null;
     createdAt: string;
     deletedAt?: string | null;
